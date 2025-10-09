7a465a2848771a71259a1e7454615b05fc8b2203
ExportDark <L22440388@mochis.tecnm.mx>
2025-10-09 07:50:32 -0700

    enviar mensaje por unicast y multicast arreglado

--- src/servidorasincrono/ServidorMulti/UnCliente.java
+++ src/servidorasincrono/ServidorMulti/UnCliente.java
@@ -13,16 +13,18 @@ import java.util.Map;
 
 public class UnCliente implements Runnable {
 
+    boolean sesionInvitado;
     int mensajesPrueba = 3;
     final DataOutputStream salida;
     final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
     final DataInputStream entrada;
-    final String id;
+    String id;
 
     public UnCliente(Socket s, String id) throws IOException {
         this.salida = new DataOutputStream(s.getOutputStream());
         this.entrada = new DataInputStream(s.getInputStream());
         this.id = id;
+        sesionInvitado = true;
     }
 
     public boolean iniciarSesion() throws IOException {
@@ -32,6 +34,7 @@ public class UnCliente implements Runnable {
         cliente.salida.writeUTF("pws: \n");
         String psw = entrada.readUTF();
         if (usr.equals("admin") && psw.equals("admin")) {//buscar en base de datos etc
+            id = usr;
             mensajesPrueba = 10000;
             return false;
 
@@ -39,17 +42,53 @@ public class UnCliente implements Runnable {
         return true;
     }
 
+    public String getQuienes(String mensaje) {
+        if (mensaje.startsWith("@")) {
+            return mensaje.replace("@", "");
+        }
+        return mensaje;
+    }
+
+    public void enviarMensajeB(String mensaje) throws IOException {
+        for (UnCliente c : ServidorMulti.clientes.values()) {
+            if (c.id != id) {
+                c.salida.writeUTF(id + " " + mensaje);
+            }
+        }
+        mensajesPrueba--;
+    }
+
+    public void enviarMensajeUM(String quienes, String mensaje) throws IOException {
+        UnCliente cliente;
+        if (quienes.contains(",")) {//multicast
+            String[] partes = quienes.split(",");
+            for (String quien : partes) {
+                cliente = ServidorMulti.clientes.get(quien);
+                cliente.salida.writeUTF(id + "->" + mensaje);
+            }
+            mensajesPrueba--;
+        }
+        cliente = ServidorMulti.clientes.get(quienes);
+        cliente.salida.writeUTF(id + "->" + mensaje);
+        mensajesPrueba--;
+    }
+
     @Override
     public void run() {
         UnCliente cliente = ServidorMulti.clientes.get(id);
         String opcion;
-        boolean sesionInvitado = true;
 
         try {
-            cliente.salida.writeUTF("Iniciar Sesion\n1.si\n2.no\n");
-            opcion = entrada.readUTF();
-            if (opcion.equals("si")) {
-                sesionInvitado = iniciarSesion();
+
+            while (true) {
+                cliente.salida.writeUTF("Iniciar Sesion\n1.si\n2.no\n");
+                opcion = entrada.readUTF();
+                if (opcion.toLowerCase().equals("si")) {
+                    sesionInvitado = iniciarSesion();
+                }
+                if (opcion.toLowerCase().equals("no")) {
+                    break;
+                }
             }
         } catch (IOException ex) {
             System.getLogger(UnCliente.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
@@ -58,41 +97,29 @@ public class UnCliente implements Runnable {
         String mensaje;
 
         while (true) {
-            try {
+
+            try {//unicast o multicast
+                cliente = ServidorMulti.clientes.get(id);
+                cliente.salida.writeUTF("Mensaje restantes: " + mensajesPrueba);
                 mensaje = entrada.readUTF();
-
-                //unicast
-                if (mensaje.startsWith("@")) {
+                if (mensaje.startsWith("@") && (mensajesPrueba > 0 || !sesionInvitado)) {
+                    cliente.salida.writeUTF("en UM...");
                     String[] partes = mensaje.split(" ");
-                    String aQuien = partes[0].substring(1);
-                    cliente = ServidorMulti.clientes.get(aQuien);
-                    if (mensajesPrueba > 0 || !sesionInvitado) {
-                        cliente.salida.writeUTF(id + " " + mensaje);
-                        mensajesPrueba--;
-                        continue;
-                    }
-
+                    enviarMensajeUM(getQuienes(partes[0]), partes[1]);
+                    cliente.salida.writeUTF("mensaje enviado a : " + getQuienes(partes[0]));
+                    continue;
                 }
-
                 //broadcast
-                if (mensajesPrueba > 0 || !sesionInvitado) {
-                    for (UnCliente c : ServidorMulti.clientes.values()) {
-                        if (c.id != id) {
-                            c.salida.writeUTF(id + " " + mensaje);
-                        }
-                    }
-                    mensajesPrueba--;
-                }
+                cliente.salida.writeUTF("en broadcast...");
+                enviarMensajeB(mensaje);
 
                 if (sesionInvitado) {
                     cliente = ServidorMulti.clientes.get(id);
-                    cliente.salida.writeUTF("te quedan " + mensajesPrueba + " mensajes");
                     if (mensajesPrueba == 0) {
                         cliente.salida.writeUTF("prueba agotada, por favor inicie sesion...!\n");
                         sesionInvitado = iniciarSesion();
                         if (sesionInvitado) {
                             cliente.salida.writeUTF("credenciales incorrectas...\n");
-                            continue;
                         }
                     }
 
